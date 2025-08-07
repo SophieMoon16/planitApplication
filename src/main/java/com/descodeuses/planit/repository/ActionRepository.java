2ce0c25cec12fa9ee9ea7386c62c5f2fdd7c0025
SophieMoon16 <sluu160593@gmail.com>
2025-08-07 11:16:05 +0200

    V17

--- src/main/java/com/descodeuses/planit/repository/ActionRepository.java
+++ src/main/java/com/descodeuses/planit/repository/ActionRepository.java
@@ -5,6 +5,6 @@ import com.descodeuses.planit.entity.ActionEntity;
 import java.util.List;
 
 public interface ActionRepository extends JpaRepository<ActionEntity, Long> {
-    List<ActionEntity> findbyUtilisateurId(Long utilisateurId);
+    /*List<ActionEntity> findbyUtilisateurId(Long utilisateurId);*/
 
 }
